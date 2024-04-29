f6537967b1e1c7a56f967b69e493dd7a3c6bbf8b
kokila555 <kokila1667@gmail.com>
2024-04-29 17:58:43 +0530

    project structure

---
+++ src/test/java/org/testtt1/SampleGit.java
@@ -0,0 +1,13 @@
+package org.testtt1;
+
+import org.testng.annotations.Test;
+
+public class SampleGit {
+
+	@Test
+	private void test() {
+		// TODO Auto-generated method stub
+
+	}
+}
+//   https://github.com/Kokila555/ProjectClassGit.git
